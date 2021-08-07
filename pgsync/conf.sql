26c51b392658a7efcd86e819d5394d1c80a92b4d
Petro Liashchynskyi <p.liashchynskyi@gmail.com>
2021-08-07 21:26:16 +0300

    final

--- pgsync/conf.sql
+++ pgsync/conf.sql
@@ -1,2 +1,4 @@
 ALTER SYSTEM SET wal_level = logical;
-ALTER SYSTEM SET max_replication_slots = 2; -- set this value to the number of tables you want to load into elastic
+ALTER SYSTEM SET max_replication_slots = 1;
+-- set this value to the number of tables you want to load into elastic
+-- for now we only have one table called product
